9b5c74c0eff99f00b605bf6b62f9ea52b5c04247
IAmDalson <105450111+IAmDalson@users.noreply.github.com>
2024-01-24 21:06:57 +0100

    Add files via upload

---
+++ Covid SQL portfolio project.sql
@@ -0,0 +1,10 @@
+SELECT TOP (1000) [continent]
+      ,[location]
+      ,[date]
+      ,[population]
+      ,[new_vaccinations]
+      ,[VaccinationSum]
+  FROM [PortfolioProject].[dbo].[PercentagePopulationVaccinated]
+
+  select *
+  from PercentagePopulationVaccinated
